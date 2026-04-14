1d3e5d69b8964a1946822a0321e068682264034f
Evan | RushWay <rushwaycontact@gmail.com>
2026-04-14 12:00:01 +0200

    Added train_set handling for gathering same data as the existing benchmark

--- launch_benchmark.py
+++ launch_benchmark.py
@@ -31,35 +31,35 @@ if not os.path.isdir(MSA_SEQ_BASE_DIR):
   os.mkdir(MSA_SEQ_BASE_DIR)
 
 
+def process_collection(data: list, collection_name: str):
+  collection_items = list(data[collection_name].items())
+
+  for component in collection_items:
+    component_name = component[0]
+    component_data = component[1]
+
+    for parent in component_data.items():
+      parent_data = parent[1]
+
+      for children in parent_data.items():
+        children_name = children[0]
+        children_data = children[1]
+
+        if not os.path.isfile(f'{NATIVE_ELEMENTS_BASE_DIR}/{children_name.lower()}.pdb'):
+          continue
+
+        sequence = children_data['sequence']
+
+        with open(f"benchmark/fasta_sequences/{children_name.lower()}.fasta", "w") as f:
+          f.write(f">{children_name}\n")
+          f.write(sequence)
+
+
 with open(FILENAME, 'r') as file:
   data = json.load(file)
 
-data_collection = data[COLLECTION]
 
-collection_items = list(data_collection.items())
-
-
-for component in collection_items:
-  component_name = component[0]
-  component_data = component[1]
-
-  for parent in component_data.items():
-    parent_data = parent[1]
-
-    for children in parent_data.items():
-      children_name = children[0]
-      children_data = children[1]
-
-      if not os.path.isfile(f'{NATIVE_ELEMENTS_BASE_DIR}/{children_name.lower()}.pdb'):
-          continue
-
-      sequence = children_data['sequence']
-      count = count + 1
-
-      with open(f"benchmark/fasta_sequences/{children_name.lower()}.fasta", "w") as f:
-        f.write(f">{children_name}\n")
-        f.write(sequence)
-
-
+process_collection(data, 'test_set')
+process_collection(data, 'train_set')
 
 os.system('sh run_predictions.sh')
